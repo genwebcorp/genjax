1166e46a076d668b47123d3425e3f9fd05234779
McCoy Becker <mccoybecker@gmail.com>
2023-04-25 22:21:01 -0400

    Local.

--- src/genjax/_src/utilities.py
+++ src/genjax/_src/utilities.py
@@ -15,14 +15,11 @@
 import jax
 import jax.numpy as jnp
 
-from genjax._src.core.typing import IntArray
 from genjax._src.core.typing import PRNGKey
 
 
 # Convenient evolution to get multiple keys.
-def slash(key: PRNGKey, num_pieces: IntArray = 1):
-    if num_pieces == 1:
-        return jax.random.split(key)
+def slash(key: PRNGKey, num_pieces):
     key, *sub_keys = jax.random.split(key, num_pieces + 1)
     sub_keys = jnp.array(sub_keys)
     return key, sub_keys
