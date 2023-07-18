(Whirlwind hotfix): fix a masking (bug in Distributions which was causing issues with correctly computing Unfold.update weights. Add appropriate test cases.

src/genjax/_src/generative_functions/combinators/vector/unfold_combinator.py
@@ -380,12 +380,23 @@ class UnfoldCombinator(GenerativeFunction, SupportsBuiltinSugar):
     ):
         new_upper = tree_diff_primal(length)
         start_lower = jnp.min(chm.indices)
+        state = tree_diff_primal(state)
+        static_args = Diff.no_change(static_args)
 
         # TODO: `UnknownChange` is used here
         # to preserve the Pytree structure across the loop.
         state_diff = jtu.tree_map(
-            lambda v: diff(v[start_lower], UnknownChange),
-            prev.get_retval(),
+            lambda v: diff(v, UnknownChange),
+            concrete_cond(
+                start_lower
+                == 0,  # if the starting index is 0, we need to grab the state argument.
+                lambda *args: state,
+                # Else, we use the retval from the previous iteration in the trace.
+                lambda *args: jtu.tree_map(
+                    lambda v: v[start_lower - 1],
+                    prev.get_retval(),
+                ),
+            ),
         )
         prev_inner_trace = prev.inner
 
@@ -403,7 +414,7 @@ class UnfoldCombinator(GenerativeFunction, SupportsBuiltinSugar):
 
             # TODO: also handle discard.
             prev = jtu.tree_map(_mutate, prev, new_tr)
-            w = w + idx_w
+            w += idx_w
 
             # TODO: c.f. message above on `UnknownChange`.
             # Preserve the diff type across the loop
@@ -419,15 +430,22 @@ class UnfoldCombinator(GenerativeFunction, SupportsBuiltinSugar):
         # TODO: add discard.
         (key, w, state_diff, new_inner_trace) = jax.lax.fori_loop(
             start_lower,
-            new_upper,
+            new_upper + 1,  # the bound semantics follow Python range semantics.
             _inner,
             (key, 0.0, state_diff, prev_inner_trace),
         )
-        retval = new_inner_trace.get_retval()
-        scores = new_inner_trace.get_score()
+
+        # Select the new return values.
+        retval = jtu.tree_map(
+            lambda v1, v2: jnp.where(
+                jnp.arange(0, self.max_length) < new_upper + 1, v1, v2
+            ),
+            new_inner_trace.get_retval(),
+            prev.get_retval(),
+        )
         args = tree_diff_primal((length, state, *static_args))
 
-        new_tr = VectorTrace(self, new_inner_trace, args, retval, jnp.sum(scores))
+        new_tr = VectorTrace(self, new_inner_trace, args, retval, prev.get_score() + w)
         return key, (state_diff, w, new_tr, EmptyChoiceMap())
 
     @dispatch

src/genjax/_src/generative_functions/combinators/vector/vector_datatypes.py
@@ -321,7 +321,8 @@ class IndexSelection(Selection):
     def get_subtrees_shallow(self):
         raise NotImplementedError
 
-    def filter(self, tree):
+    @typecheck
+    def filter(self, tree: ChoiceMap):
         filtered = self.inner.filter(tree)
         flags = jnp.logical_and(
             self.indices >= 0,

src/genjax/_src/generative_functions/distributions/distribution.py
@@ -17,7 +17,6 @@ import abc
 from dataclasses import dataclass
 
 import jax
-import jax.numpy as jnp
 import jax.tree_util as jtu
 
 from genjax._src.core.datatypes.generative import AllSelection
@@ -36,6 +35,7 @@ from genjax._src.core.interpreters.staging import concrete_cond
 from genjax._src.core.transforms.incremental import Diff
 from genjax._src.core.transforms.incremental import NoChange
 from genjax._src.core.transforms.incremental import UnknownChange
+from genjax._src.core.transforms.incremental import diff
 from genjax._src.core.transforms.incremental import static_check_no_change
 from genjax._src.core.transforms.incremental import static_check_tree_leaves_diff
 from genjax._src.core.transforms.incremental import tree_diff_primal
@@ -44,6 +44,7 @@ from genjax._src.core.typing import FloatArray
 from genjax._src.core.typing import List
 from genjax._src.core.typing import PRNGKey
 from genjax._src.core.typing import Tuple
+from genjax._src.core.typing import dispatch
 from genjax._src.core.typing import typecheck
 from genjax._src.generative_functions.builtin.builtin_gen_fn import SupportsBuiltinSugar
 
@@ -178,107 +179,71 @@ class Distribution(GenerativeFunction, SupportsBuiltinSugar):
             DistributionTrace(self, args, v, score),
         )
 
-    # NOTE: Here's an interesting note about `update`...
-    # (really, any of the GFI methods for any generative function)
-    # - they should return homogeneous types for any return
-    # branch leading out of the call.
-    # Because these methods may be invoked in `jax.lax.switch` calls
-    # it's important that callers have some knowledge about the
-    # consistency of invoking a callee -- most generative function
-    # languages ensure this is true by default e.g. if they defer
-    # some of their behavior to callees.
-    # For `Distribution` this is not true by default - we have to be
-    # careful when defining the methods, and this is most true of update
-    # below.
-    @typecheck
+    @dispatch
     def update(
         self,
         key: PRNGKey,
         prev: DistributionTrace,
-        constraints: ChoiceMap,
+        constraints: EmptyChoiceMap,
         argdiffs: Tuple,
         **kwargs
     ) -> Tuple[PRNGKey, Tuple[Any, FloatArray, DistributionTrace, Any]]:
-        assert isinstance(constraints, Leaf)
         static_check_tree_leaves_diff(argdiffs)
-        maybe_discard = mask(False, prev.get_choices())
+        v = prev.get_retval()
+        retval_diff = jtu.tree_map(lambda v: Diff(v, NoChange), v)
+        discard = mask(False, prev.get_choices())
 
-        # Incremental optimization - if nothing has changed,
-        # just return the previous trace.
-        if isinstance(constraints, EmptyChoiceMap) and static_check_no_change(argdiffs):
-            v = prev.get_retval()
-            retval_diff = jtu.tree_map(lambda v: Diff(v, NoChange), v)
-            return key, (retval_diff, 0.0, prev, maybe_discard)
+        # If no change to arguments, no need to update.
+        if static_check_no_change(argdiffs):
+            return key, (retval_diff, 0.0, prev, discard)
 
-        # Otherwise, we consider the cases.
-        args = tree_diff_primal(argdiffs)
-
-        # First, we have to check if the trace provided
-        # is masked or not. It's possible that a trace
-        # with a mask is updated.
-        prev_v = prev.get_retval()
-        active = True
-        if isinstance(prev_v, BooleanMask):
-            active = prev_v.mask
-            prev_v = prev_v.unmask()
-
-        # Case 1: the new choice map is empty here.
-        if isinstance(constraints, EmptyChoiceMap):
-            prev_score = prev.get_score()
-            v = prev_v
-
-            # If the value is active, we compute any weight
-            # corrections from changing arguments.
-            def _active(key, v, *args):
-                key, fwd = self.estimate_logpdf(key, v, *args)
-                return key, fwd - prev_score
-
-            # If the value is inactive, we do nothing.
-            def _inactive(key, v, *args):
-                return key, prev_score
-
-            key, w = concrete_cond(active, _active, _inactive, key, v, *args)
-            discard = maybe_discard
-            retval_diff = jtu.tree_map(lambda v: Diff(v, NoChange), prev_v)
-
-        # Case 2: the new choice map is not empty here.
+        # Otherwise, we must compute an incremental weight.
         else:
-            prev_score = prev.get_score()
-            v = constraints.get_leaf_value()
-
-            # Now, we must check if the choice map has a masked
-            # leaf value, and dispatch accordingly.
-            active_chm = True
-            if isinstance(v, BooleanMask):
-                active_chm = v.mask
-                v = v.unmask()
-
-            # The only time this flag is on is when both leaf values
-            # are concrete, or they are both masked with true mask
-            # values.
-            active = jnp.all(jnp.logical_and(active_chm, active))
-
+            args = tree_diff_primal(argdiffs)
             key, fwd = self.estimate_logpdf(key, v, *args)
+            bwd = prev.get_score()
+            return key, (retval_diff, fwd - bwd, prev, discard)
 
-            def _constraints_active(key, v, *args):
-                return key, v, fwd - prev_score
+    @dispatch
+    def update(
+        self,
+        key: PRNGKey,
+        prev: DistributionTrace,
+        constraints: ValueChoiceMap,
+        argdiffs: Tuple,
+        **kwargs
+    ) -> Tuple[PRNGKey, Tuple[Any, FloatArray, DistributionTrace, Any]]:
+        static_check_tree_leaves_diff(argdiffs)
+        args = tree_diff_primal(argdiffs)
+        v = constraints.get_leaf_value()
+        if isinstance(v, BooleanMask):
+            check, value = v.mask, v.value
 
-            def _constraints_inactive(key, v, *args):
-                return key, prev_v, 0.0
+            def _fallback(key):
+                key, (retdiff, w, new_tr, discard) = self.update(
+                    key, prev, EmptyChoiceMap(), argdiffs
+                )
+                primal = tree_diff_primal(retdiff)
+                # Because we are pushing the update to depend dynamically on a mask flag value,
+                # we have to ensure that all branches return the same types of Pytree.
+                coerce_to_unknown = jtu.tree_map(
+                    lambda v: diff(v, UnknownChange), primal
+                )
+                return key, (coerce_to_unknown, w, new_tr, discard)
 
-            key, v, w = concrete_cond(
-                active_chm, _constraints_active, _constraints_inactive, key, v, *args
-            )
+            def _active(key):
+                return self.update(key, prev, ValueChoiceMap(value), argdiffs)
 
-            discard = mask(active_chm, ValueChoiceMap(prev.get_leaf_value()))
-            retval_diff = jtu.tree_map(lambda v: Diff(v, UnknownChange), v)
+            return concrete_cond(check, _active, _fallback, key)
+        else:
+            key, fwd = self.estimate_logpdf(key, v, *args)
+            bwd = prev.get_score()
+            w = fwd - bwd
+            new_tr = DistributionTrace(self, args, v, fwd)
+            discard = mask(True, prev.get_choices())
+            retval_diff = jtu.tree_map(lambda x: diff(x, UnknownChange), v)
 
-        return key, (
-            retval_diff,
-            w,
-            DistributionTrace(self, args, v, w),
-            discard,
-        )
+            return key, (retval_diff, w, new_tr, discard)
 
     @typecheck
     def assess(

tests/generative_functions/combinators/vector/test_unfold.py
@@ -141,10 +141,53 @@ class TestUnfoldSimpleNormal:
             init_key, obs_chm(_y, 0), (0, model_args)
         )
 
+        # Checks updates.
         diffs = (
             diff(1, UnknownChange),
             jtu.tree_map(lambda v: diff(v, NoChange), model_args),
         )
 
-        _ = model.update(update_key, init_tr, obs_chm(_y, 1), diffs)
-        assert True
+        key, (_, w, new_tr, _) = model.update(
+            update_key, init_tr, obs_chm(_y, 1), diffs
+        )
+        assert init_tr.get_score() + w == new_tr.get_score()
+
+        diffs = (
+            diff(2, UnknownChange),
+            jtu.tree_map(lambda v: diff(v, NoChange), model_args),
+        )
+
+        key, (_, w, new_new_tr, _) = model.update(
+            update_key, new_tr, obs_chm(_y, 2), diffs
+        )
+        assert new_tr.get_score() + w == new_new_tr.get_score()
+
+    def test_check_weight_computations(self):
+        @genjax.gen(genjax.Unfold, max_length=10)
+        def chain(z_prev):
+            z = genjax.normal(z_prev, 1.0) @ "z"
+            x = genjax.normal(z, 1.0) @ "x"
+            return z
+
+        key = jax.random.PRNGKey(314159)
+        key, tr = chain.simulate(key, (5, 0.0))
+
+        #####
+        # Check specific weight computations.
+        #####
+
+        # Ensure that update is computed correctly.
+        for t in range(0, 5):
+            obs = genjax.index_choice_map(
+                [t],
+                genjax.choice_map({"x": jnp.array([1.0])}),
+            )
+            diffs = (genjax.diff(5, NoChange), genjax.diff(0.0, NoChange))
+            key, (_, w, new_tr, _) = chain.update(key, tr, obs, diffs)
+            sel = genjax.index_select([t], genjax.select("z"))
+            z = sel.filter(new_tr.strip())["z"]
+            x_sel = genjax.index_select([t], genjax.select("x"))
+            assert new_tr.project(x_sel) == pytest.approx(
+                genjax.normal.logpdf(1.0, z, 1.0), 0.0001
+            )
+            assert w == pytest.approx(new_tr.project(x_sel) - tr.project(x_sel), 0.0001)