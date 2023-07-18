4451e463c29ff966f3c166a1e7ec89328f0aaba1
McCoy Becker <mccoybecker@gmail.com>
2023-07-18 17:05:59 -0400

    (Whirlwind hotfix): fix a masking (bug in Distributions which was causing issues with correctly computing Unfold.update weights. Add appropriate test cases.

--- src/genjax/_src/core/transforms/harvest.py
+++ src/genjax/_src/core/transforms/harvest.py
@@ -251,7 +251,9 @@ class HarvestTrace(jc.Trace):
         if primitive is sow_p:
             outvals = context.process_sow(*vals, **params)
             return jax_util.safe_map(self.pure, outvals)
-        outvals = primitive.bind(*vals, **params)
+        subfuns, params = primitive.get_bind_params(params)
+        args = subfuns + vals
+        outvals = primitive.bind(*args, **params)
         if not primitive.multiple_results:
             outvals = [outvals]
         out_tracers = jax_util.safe_map(self.pure, outvals)
