4a17d5e4d4b2704d98d516032679a39c999d7ede
McCoy Becker <mccoybecker@gmail.com>
2023-08-10 09:09:06 -0400

    Add tagged union.

--- src/genjax/_src/core/datatypes/tagged_unions.py
+++ src/genjax/_src/core/datatypes/tagged_unions.py
@@ -14,10 +14,16 @@
 
 from dataclasses import dataclass
 
+import jax
+import jax.tree_util as jtu
+
+from genjax._src.core.datatypes.address_tree import AddressLeaf
 from genjax._src.core.pytree.pytree import Pytree
 from genjax._src.core.typing import Any
+from genjax._src.core.typing import Callable
 from genjax._src.core.typing import IntArray
 from genjax._src.core.typing import List
+from genjax._src.core.typing import typecheck
 
 
 @dataclass
@@ -26,7 +32,36 @@ class TaggedUnion(Pytree):
     values: List[Any]
 
     def flatten(self):
-        return (self.tag, self.value), ()
+        return (self.tag, self.values), ()
+
+    @classmethod
+    def new(cls, tag: IntArray, values: List[Any]):
+        return cls(tag, values).leaf_push()
+
+    def _set_leafs(self, *vs: AddressLeaf):
+        leaf_values = list(map(lambda v: v.get_leaf_value(), vs))
+        first = vs[0]
+        return first.set_leaf_value(TaggedUnion(self.tag, leaf_values))
+
+    def leaf_push(self):
+        def _inner(*v):
+            # `AddressLeaf` inheritors have a method `set_leaf_value`
+            # to participate in tagging.
+            # They can choose how to construct themselves after
+            # being provided with a tagged value.
+            if all(map(lambda v: isinstance(v, AddressLeaf), v)):
+                return self._set_leafs(*v)
+            else:
+                return list(v)
+
+        def _check(v):
+            return isinstance(v, AddressLeaf)
+
+        return jtu.tree_map(_inner, *self.values, is_leaf=_check)
+
+    @typecheck
+    def switch(self, *callables: Callable):
+        return jax.lax.switch(self.tag, *callables)
 
 
 ##############
