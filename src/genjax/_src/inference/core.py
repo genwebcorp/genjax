4a9d0c61d5eb4ff711379ff5564842368dd6434b
femtomc <mccoybecker@gmail.com>
2024-01-31 14:10:43 -0500

    Add docstring linter via ruff options.

--- src/genjax/_src/inference/core.py
+++ src/genjax/_src/inference/core.py
@@ -57,11 +57,11 @@ class Target(Pytree):
 
 
 class InferenceAlgorithm(Pytree):
-    """The class `InferenceAlgorithm` represents the type of inference algorithms,
-    programs which implement interfaces for sampling from approximate posterior
-    representations, and estimating the density of the approximate posterior.
+    """The abstract class `InferenceAlgorithm` represents the type of inference
+    algorithms, programs which implement interfaces for sampling from approximate
+    posterior representations, and estimating the density of the approximate posterior.
 
-    `InferenceAlgorithm` implementors can also implement two optional methods designed to support effective gradient estimators for variational objectives (`estimate_normalizing_constant` and `estimate_reciprocal_normalizing_constant`).
+    Subclasses of type `InferenceAlgorithm` can also implement two optional methods designed to support effective gradient estimators for variational objectives (`estimate_normalizing_constant` and `estimate_reciprocal_normalizing_constant`).
     """
 
     #########
