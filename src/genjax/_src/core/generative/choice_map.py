320f483f93819933edb1f75462db8698dc4a892a
McCoy Becker <mccoybecker@gmail.com>
2024-05-22 15:49:28 -0700

    Fix inversion issue in AddrFnChmFn.

--- src/genjax/_src/core/generative/choice_map.py
+++ src/genjax/_src/core/generative/choice_map.py
@@ -976,6 +976,8 @@ class AddrFnChmFn(ChoiceMapFunction):
             return self.c.get_submap(mapped).get_submap(addr)
         else:
             mapped = self.addr_fn.get(addr, addr)
+            if mapped is ...:
+                return self.c
             return self.c.get_submap(mapped)
 
 
