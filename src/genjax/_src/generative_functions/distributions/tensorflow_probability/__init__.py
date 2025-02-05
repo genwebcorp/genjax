f0c9b5642bca6e961dad6c6338930044157eef7f
Mathieu Huot <MathieuHuot@users.noreply.github.com>
2025-02-04 21:51:41 -0500

    Adding some TFP distributions (initially Gamma), a test, nuking Bates as it doesn't work (#1493)

--- src/genjax/_src/generative_functions/distributions/tensorflow_probability/__init__.py
+++ src/genjax/_src/generative_functions/distributions/tensorflow_probability/__init__.py
@@ -61,31 +61,39 @@ def tfp_distribution(
 #####################
 # Wrapper instances #
 #####################
-
-beta = tfp_distribution(tfd.Beta)
-
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.Beta`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Beta) distribution from TensorFlow Probability distributions.
-"""
-
-bates = tfp_distribution(tfd.Bates)
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.Bates`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Bates) distribution from TensorFlow Probability distributions.
-"""
-
 bernoulli = tfp_distribution(lambda logits: tfd.Bernoulli(logits=logits))
 """
 A `tfp_distribution` generative function which wraps the [`tfd.Bernoulli`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Bernoulli) distribution from TensorFlow Probability distributions.
 """
 
-flip = tfp_distribution(lambda p: tfd.Bernoulli(probs=p, dtype=jnp.bool_))
+beta = tfp_distribution(tfd.Beta)
 """
-A `tfp_distribution` generative function which wraps the [`tfd.Bernoulli`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Bernoulli) distribution from TensorFlow Probability distributions, but is constructed using a probability value and not a logit.
+A `tfp_distribution` generative function which wraps the [`tfd.Beta`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Beta) distribution from TensorFlow Probability distributions.
 """
 
-exponential = tfp_distribution(tfd.Exponential)
+beta_binomial = tfp_distribution(tfd.BetaBinomial)
 """
-A `tfp_distribution` generative function which wraps the [`tfd.Exponential`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Exponential) distribution from TensorFlow Probability distributions, but is constructed using a probability value and not a logit.
+A `tfp_distribution` generative function which wraps the [`tfd.BetaBinomial`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/BetaBinomial) distribution from TensorFlow Probability distributions.
+"""
+
+beta_quotient = tfp_distribution(tfd.BetaQuotient)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.BetaQuotient`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/BetaQuotient) distribution from TensorFlow Probability distributions.
+"""
+
+binomial: ExactDensityFromCallables[Array] = tfp_distribution(tfd.Binomial)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Binomial`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Binomial) distribution from TensorFlow Probability distributions.
+"""
+
+categorical = tfp_distribution(lambda logits: tfd.Categorical(logits=logits))
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Categorical`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Categorical) distribution from TensorFlow Probability distributions.
+"""
+
+cauchy = tfp_distribution(tfd.Cauchy)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Cauchy`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Cauchy) distribution from TensorFlow Probability distributions.
 """
 
 chi = tfp_distribution(tfd.Chi)
@@ -101,6 +109,41 @@ A `tfp_distribution` generative function which wraps the [`tfd.Chi2`](https://ww
 dirichlet = tfp_distribution(tfd.Dirichlet)
 """
 A `tfp_distribution` generative function which wraps the [`tfd.Dirichlet`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Dirichlet) distribution from TensorFlow Probability distributions.
+"""
+
+dirichlet_multinomial = tfp_distribution(tfd.DirichletMultinomial)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.DirichletMultinomial`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Dirichlet) distribution from TensorFlow Probability distributions.
+"""
+
+double_sided_maxwell = tfp_distribution(tfd.DoublesidedMaxwell)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.DoublesidedMaxwell`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/DoublesidedMaxwell) distribution from TensorFlow Probability distributions.
+"""
+
+exp_gamma = tfp_distribution(tfd.ExpGamma)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.ExpGamma`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/ExpGamma) distribution from TensorFlow Probability distributions.
+"""
+
+exp_inverse_gamma = tfp_distribution(tfd.ExpInverseGamma)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.ExpInverseGamma`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/ExpInverseGamma) distribution from TensorFlow Probability distributions.
+"""
+
+exponential = tfp_distribution(tfd.Exponential)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Exponential`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Exponential) distribution from TensorFlow Probability distributions.
+"""
+
+flip = tfp_distribution(lambda p: tfd.Bernoulli(probs=p, dtype=jnp.bool_))
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Bernoulli`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Bernoulli) distribution from TensorFlow Probability distributions, but is constructed using a probability value and not a logit.
+"""
+
+gamma = tfp_distribution(tfd.Gamma)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Gamma`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Gamma) distribution from TensorFlow Probability distributions.
 """
 
 geometric = tfp_distribution(tfd.Geometric)
@@ -133,14 +176,29 @@ inverse_gamma = tfp_distribution(tfd.InverseGamma)
 A `tfp_distribution` generative function which wraps the [`tfd.InverseGamma`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/InverseGamma) distribution from TensorFlow Probability distributions.
 """
 
+inverse_gaussian = tfp_distribution(tfd.InverseGaussian)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.InverseGaussian`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/InverseGaussian) distribution from TensorFlow Probability distributions.
+"""
+
 kumaraswamy = tfp_distribution(tfd.Kumaraswamy)
 """
 A `tfp_distribution` generative function which wraps the [`tfd.Kumaraswamy`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Kumaraswamy) distribution from TensorFlow Probability distributions.
 """
 
+lambert_w_normal = tfp_distribution(tfd.LambertWNormal)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.LambertWNormal`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/LambertWNormal) distribution from TensorFlow Probability distributions.
+"""
+
 laplace = tfp_distribution(tfd.Laplace)
 """
 A `tfp_distribution` generative function which wraps the [`tfd.Laplace`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Laplace) distribution from TensorFlow Probability distributions.
+"""
+
+log_normal = tfp_distribution(tfd.LogNormal)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.LogNormal`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/LogNormal) distribution from TensorFlow Probability distributions.
 """
 
 logit_normal = tfp_distribution(tfd.LogitNormal)
@@ -158,14 +216,34 @@ multinomial = tfp_distribution(tfd.Multinomial)
 A `tfp_distribution` generative function which wraps the [`tfd.Multinomial`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Multinomial) distribution from TensorFlow Probability distributions.
 """
 
+mv_normal_diag = tfp_distribution(tfd.MultivariateNormalDiag)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.MultivariateNormalDiag`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/MultivariateNormalDiag) distribution from TensorFlow Probability distributions.
+"""
+
+mv_normal = tfp_distribution(tfd.MultivariateNormalFullCovariance)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.MultivariateNormalFullCovariance`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/MultivariateNormalFullCovariance) distribution from TensorFlow Probability distributions.
+"""
+
 negative_binomial = tfp_distribution(tfd.NegativeBinomial)
 """
 A `tfp_distribution` generative function which wraps the [`tfd.NegativeBinomial`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/NegativeBinomial) distribution from TensorFlow Probability distributions.
 """
 
-plackett_luce = tfp_distribution(tfd.PlackettLuce)
+non_central_chi2 = tfp_distribution(tfd.NoncentralChi2)
 """
-A `tfp_distribution` generative function which wraps the [`tfd.PlackettLuce`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/PlackettLuce) distribution from TensorFlow Probability distributions.
+A `tfp_distribution` generative function which wraps the [`tfd.NoncentralChi2`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/NoncentralChi2) distribution from TensorFlow Probability distributions.
+"""
+
+normal = tfp_distribution(tfd.Normal)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Normal`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Normal) distribution from TensorFlow Probability distributions.
+"""
+
+poisson = tfp_distribution(tfd.Poisson)
+"""
+A `tfp_distribution` generative function which wraps the [`tfd.Poisson`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Poisson) distribution from TensorFlow Probability distributions.
 """
 
 power_spherical = tfp_distribution(tfd.PowerSpherical)
@@ -181,28 +259,6 @@ A `tfp_distribution` generative function which wraps the [`tfd.Skellam`](https:/
 student_t = tfp_distribution(tfd.StudentT)
 """
 A `tfp_distribution` generative function which wraps the [`tfd.StudentT`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/StudentT) distribution from TensorFlow Probability distributions.
-"""
-
-normal = tfp_distribution(tfd.Normal)
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.Normal`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Normal) distribution from TensorFlow Probability distributions.
-"""
-
-mv_normal_diag = tfp_distribution(
-    lambda μ, Σ_diag: tfd.MultivariateNormalDiag(loc=μ, scale_diag=Σ_diag)
-)
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.MultivariateNormalDiag`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/MultivariateNormalDiag) distribution from TensorFlow Probability distributions.
-"""
-
-mv_normal = tfp_distribution(tfd.MultivariateNormalFullCovariance)
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.MultivariateNormalFullCovariance`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/MultivariateNormalFullCovariance) distribution from TensorFlow Probability distributions.
-"""
-
-categorical = tfp_distribution(lambda logits: tfd.Categorical(logits=logits))
-"""
-A `tfp_distribution` generative function which wraps the [`tfd.Categorical`](https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/Categorical) distribution from TensorFlow Probability distributions.
 """
 
 truncated_cauchy = tfp_distribution(tfd.TruncatedCauchy)
--- tests/generative_functions/test_distributions.py
+++ tests/generative_functions/test_distributions.py
@@ -189,3 +189,284 @@ class TestDistributions:
             == genjax.normal.assess(tr.get_choices(), (1.0, 1.0))[0]
             - genjax.normal.assess(tr.get_choices(), (0.0, 1.0))[0]
         )
+
+    def test_using_primitive_distributions(self):
+        @genjax.gen
+        def model():
+            _ = (
+                genjax.bernoulli(
+                    0.5,
+                )
+                @ "a"
+            )
+            _ = genjax.beta(1.0, 1.0) @ "b"
+            _ = genjax.beta_binomial(1.0, 1.0, 1.0) @ "c"
+            _ = genjax.beta_quotient(1.0, 1.0, 1.0, 1.0) @ "d"
+            _ = genjax.binomial(1.0, 0.5) @ "e"
+            _ = (
+                genjax.cauchy(
+                    0.0,
+                    1.0,
+                )
+                @ "f"
+            )
+            _ = (
+                genjax.categorical(
+                    [0.5, 0.5],
+                )
+                @ "g"
+            )
+            _ = (
+                genjax.chi(
+                    1.0,
+                )
+                @ "h"
+            )
+            _ = (
+                genjax.chi2(
+                    1.0,
+                )
+                @ "i"
+            )
+            _ = (
+                genjax.dirichlet(
+                    [
+                        1.0,
+                        1.0,
+                    ],
+                )
+                @ "j"
+            )
+            _ = (
+                genjax.dirichlet_multinomial(
+                    1.0,
+                    [
+                        1.0,
+                        1.0,
+                    ],
+                )
+                @ "k"
+            )
+            _ = genjax.double_sided_maxwell(1.0, 1.0) @ "l"
+            _ = (
+                genjax.exp_gamma(
+                    1.0,
+                    1.0,
+                )
+                @ "m"
+            )
+            _ = (
+                genjax.exp_inverse_gamma(
+                    1.0,
+                    1.0,
+                )
+                @ "n"
+            )
+            _ = (
+                genjax.exponential(
+                    1.0,
+                )
+                @ "o"
+            )
+            _ = (
+                genjax.flip(
+                    0.5,
+                )
+                @ "p"
+            )
+            _ = (
+                genjax.gamma(
+                    1.0,
+                    1.0,
+                )
+                @ "q"
+            )
+            _ = (
+                genjax.geometric(
+                    0.5,
+                )
+                @ "r"
+            )
+            _ = (
+                genjax.gumbel(
+                    0.0,
+                    1.0,
+                )
+                @ "s"
+            )
+            _ = genjax.half_cauchy(1.0, 1.0) @ "t"
+            _ = genjax.half_normal(1.0) @ "u"
+            _ = genjax.half_student_t(1.0, 1.0, 1.0) @ "v"
+            _ = (
+                genjax.inverse_gamma(
+                    1.0,
+                    1.0,
+                )
+                @ "w"
+            )
+            _ = (
+                genjax.kumaraswamy(
+                    1.0,
+                    1.0,
+                )
+                @ "x"
+            )
+            _ = (
+                genjax.laplace(
+                    0.0,
+                    1.0,
+                )
+                @ "y"
+            )
+            _ = (
+                genjax.lambert_w_normal(
+                    1.0,
+                    1.0,
+                )
+                @ "z"
+            )
+            _ = (
+                genjax.log_normal(
+                    0.0,
+                    1.0,
+                )
+                @ "aa"
+            )
+            _ = (
+                genjax.logit_normal(
+                    0.0,
+                    1.0,
+                )
+                @ "bb"
+            )
+            _ = (
+                genjax.moyal(
+                    0.0,
+                    1.0,
+                )
+                @ "cc"
+            )
+            _ = (
+                genjax.multinomial(
+                    1.0,
+                    [0.5, 0.5],
+                )
+                @ "dd"
+            )
+            _ = (
+                genjax.mv_normal(
+                    [0.0, 0.0],
+                    [[1.0, 0.0], [0.0, 1.0]],
+                )
+                @ "ee"
+            )
+            _ = (
+                genjax.mv_normal_diag(
+                    jnp.array([1.0, 1.0]),
+                )
+                @ "ff"
+            )
+            _ = (
+                genjax.negative_binomial(
+                    1.0,
+                    0.5,
+                )
+                @ "gg"
+            )
+            _ = (
+                genjax.non_central_chi2(
+                    1.0,
+                    1.0,
+                )
+                @ "hh"
+            )
+            _ = (
+                genjax.normal(
+                    0.0,
+                    1.0,
+                )
+                @ "ii"
+            )
+            _ = (
+                genjax.poisson(
+                    1.0,
+                )
+                @ "kk"
+            )
+            _ = (
+                genjax.power_spherical(
+                    jnp.array([
+                        0.0,
+                        0.0,
+                    ]),
+                    1.0,
+                )
+                @ "ll"
+            )
+            _ = (
+                genjax.skellam(
+                    1.0,
+                    1.0,
+                )
+                @ "mm"
+            )
+            _ = genjax.student_t(1.0, 1.0, 1.0) @ "nn"
+            _ = (
+                genjax.truncated_cauchy(
+                    0.0,
+                    1.0,
+                    0.0,
+                    1.0,
+                )
+                @ "oo"
+            )
+            _ = (
+                genjax.truncated_normal(
+                    0.0,
+                    1.0,
+                    0.0,
+                    1.0,
+                )
+                @ "pp"
+            )
+            _ = (
+                genjax.uniform(
+                    0.0,
+                    1.0,
+                )
+                @ "qq"
+            )
+            _ = (
+                genjax.von_mises(
+                    0.0,
+                    1.0,
+                )
+                @ "rr"
+            )
+            _ = (
+                genjax.von_mises_fisher(
+                    jnp.array([
+                        0.0,
+                        0.0,
+                    ]),
+                    1.0,
+                )
+                @ "ss"
+            )
+            _ = (
+                genjax.weibull(
+                    1.0,
+                    1.0,
+                )
+                @ "tt"
+            )
+            _ = (
+                genjax.zipf(
+                    2.0,
+                )
+                @ "uu"
+            )
+            return None
+
+        key = jax.random.key(314159)
+        _ = model.simulate(key, ())
